3a1f16cad595ceaa8be6bd2f8bb4b5add59b863e
MarcusTw <60913700+MarcusTw@users.noreply.github.com>
2021-05-23 22:21:12 +0800

    Fix UI bug

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Dimensions, StyleSheet, Text, View, SafeAreaView, TextInput, Platform, Button, PixelRatio   } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  TextInput,
+  Platform,
+  Button,
+  PixelRatio,
+} from "react-native";
+//import { Constants } from "Constants.js";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style = {{fontSize: 30, fontWeight: 'bold'}}>Dealshare</Text>
+      <Text style={{ fontSize: 30, fontWeight: "bold" }}>Dealshare</Text>
       <StatusBar style="auto" />
-      <SafeAreaView style = {[styles.loginMenu, {width: PixelRatio.getPixelSizeForLayoutSize(300), height: PixelRatio.getPixelSizeForLayoutSize(400), marginTop: "10%", padding: 20}]}>
-        <Text style = {{fontWeight: 'bold', fontSize: 30, textAlign: 'center'}}>Sign up</Text>
-        <TextInput style = {styles.inputBox} placeholder = "Username"></TextInput> 
-        <TextInput style = {styles.inputBox} placeholder = "Email"></TextInput>
-        <TextInput style = {styles.inputBox} placeholder = "Password"></TextInput>
-        <TextInput style = {styles.inputBox} placeholder = "Re-enter password"></TextInput> 
-        <View style = {{width: "70%", height: "20%", alignSelf: "center", paddingTop: "5%"}}>
-          <Button title = "Register" color = "red"/>
+      <SafeAreaView
+        style={[
+          styles.loginMenu,
+          {
+            width: PixelRatio.getPixelSizeForLayoutSize(120),
+            height: PixelRatio.getPixelSizeForLayoutSize(170),
+            marginTop: "10%",
+            padding: 20,
+          },
+        ]}
+      >
+        <Text style={{ fontWeight: "bold", fontSize: 30, textAlign: "center" }}>
+          Sign up
+        </Text>
+        <TextInput style={styles.inputBox} placeholder="Username"></TextInput>
+        <TextInput style={styles.inputBox} placeholder="Email"></TextInput>
+        <TextInput style={styles.inputBox} placeholder="Password"></TextInput>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Re-enter password"
+        ></TextInput>
+        <View
+          style={{
+            width: "70%",
+            height: "20%",
+            alignSelf: "center",
+            paddingTop: "5%",
+          }}
+        >
+          <Button title="Register" color="red" />
         </View>
       </SafeAreaView>
     </SafeAreaView>
@@ -24,25 +57,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
-    alignItems: 'center',
-    justifyContent: 'flex-top',
+    backgroundColor: "dodgerblue",
+    alignItems: "center",
+    justifyContent: "center",
   },
   loginMenu: {
-    backgroundColor: 'yellow',
-    justifyContent: 'flex-top',
-    borderRadius: 10
-  
+    backgroundColor: "yellow",
+    justifyContent: "center",
+    borderRadius: 10,
   },
   inputBox: {
     borderRadius: 25,
     marginVertical: 10,
-    width: "70%",
-    height: "10%", 
+    width: "90%",
+    height: "10%",
     borderColor: "black",
-    borderWidth: 1, 
-    paddingLeft: 10, 
+    borderWidth: 1,
+    paddingLeft: 10,
     marginTop: "5%",
-    alignSelf: 'center'
+    alignSelf: "center",
   },
 });
